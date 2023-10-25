92ca84eede21442c3d1c26c56ef4f8c5622a410d
Pattara <pattaraw.to@gmail.com>
2023-10-25 23:45:04 +0700

    addyt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const theButton = document.getElementById("theButton");
 
-function random() {
+function ranom() {
     let posX = Math.floor(Math.random() * 10) * 10;
     let posY = Math.floor(Math.random() * 10) * 10;
 
@@ -10,4 +10,5 @@ function random() {
 
 function catched() {
     alert("All right, I give up. You win.")
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 }
